1327745d41ec8a919e3c528dffad5e7a7d7557be
Ashton Christie <achristie@corp.validar.com>
2019-02-01 16:24:02 -0800

    onto chap 10

---
+++ chap7.js
@@ -0,0 +1,78 @@
+﻿// way to map out path from node to node.
+const graph = {};
+graph['start'] = {};
+graph['start']['a'] = 6;
+graph['start']['b'] = 2;
+graph['a'] = {};
+graph['a']['fin'] = 1;
+graph['b'] = {};
+graph['b']['a'] = 3;
+graph['b']['fin'] = 5;
+graph['fin'] = {};
+
+// console.log(Object.keys(graph['start']));
+// console.log(graph['start']['a']);
+// console.log(graph);
+
+// way to keep track of cost (time, distance, value, etc) to get to node.
+const costs = {};
+costs['a'] = 6;
+costs['b'] = 2;
+costs['fin'] = Infinity;
+
+// way to keep track of best path;
+const parents = {};
+parents['a'] = 'start';
+parents['b'] = 'start';
+parents['fin'] = null;
+
+// need a way to keep track of all nodes you process
+const processed = [];
+
+// 1. While we have nodes to process..
+// 2. Grab the node that is closest to the start
+// 3. Update Costs for it's neighbors
+// 4. If any of the neighbors costs were updated, update the parent's too
+// 5. Mark this node processed. Repeat.
+
+function findLowestCostNode(costs) {
+  let lowestCost = Infinity;
+  let lowestCostNode = null;
+  for (node in costs) {
+    // go through each node
+    let cost = costs[node];
+    if (cost < lowestCost && !processed.includes(node)) {
+      // if it's the lowest cost so far and hasn't been processed yet..
+      lowestCost = cost; // set as lowest cost node
+      lowestCostNode = node;
+    }
+  }
+  return lowestCostNode;
+}
+
+function dijkstrasAlgo(graph, costs, parents) {
+  // find the lowest cost node that you haven't processed yet
+  let node = findLowestCostNode(costs);
+  // if you've processed all the nodes, this while loop is done
+  while (node !== null) {
+    let cost = costs[node];
+    let neighbors = graph[node];
+    // go through all neighbors of this node
+    for (n in Object.keys(neighbors)) {
+      let newCost = cost + neighbors[n];
+      // if its cheaper to get to this neighbor by going through this node..
+      if (costs[n] > newCost) {
+        // update the cost for this node
+        costs[n] = newCost;
+        // this node becomes the new parent for this neighbor
+        parents[n] = node;
+      }
+    }
+    // mark this node as processed
+    processed.push(node);
+    // find the next node to process and loop
+    node = findLowestCostNode(costs);
+  }
+}
+
+dijkstrasAlgo(graph, costs, parents);
---
+++ chap8.js
@@ -0,0 +1,45 @@
+﻿//? 8.1 - You work for a furniture company, and you have to ship furniture all over the country. You need to pack your truck with boxes. All the boxes are of different sizes, and you’re trying to maximize the space you use in each truck. How would you pick boxes to maximize space? Come up with a greedy strategy. Will that give you the optimal solution?
+
+//! Put in the biggest box that you can and repeat. When you can no longer fit in the biggest box, put in the next biggest box that will fit and repeat until no more boxes will fit.
+
+//? 8.2 - You’re going to Europe, and you have seven days to see everything you can. You assign a point value to each item (how much you want to see it) and estimate how long it takes. How can you maximize the point total (seeing all the things you really want to see) during your stay? Come up with a greedy strategy. Will that give you the optimal solution?
+
+//! make a list of all activites, then assign a value to each one that is point val/time to see. Sort list and start doing highest value items first.
+
+// A greedy algorithm is simple: at each step, pick the optimal move. In this case, each time you pick         a class, you pick the class that ends the soonest. In technical terms: at each step you pick the locally optimal solution, and in the end you’re left with the globally optimal solution.
+
+let statesNeeded = new Set(['mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az']);
+
+let stations = {};
+stations['kone'] = new Set(['id', 'nv', 'ut']);
+stations['ktwo'] = new Set(['wa', 'id', 'mt']);
+stations['kthree'] = new Set(['or', 'nv', 'ca']);
+stations['kfour'] = new Set(['nv', 'ut']);
+stations['kfive'] = new Set(['ca', 'az']);
+
+function findStations(statesNeeded, stations) {
+  let finalStations = new Set();
+
+  while (statesNeeded) {
+    let bestStation = null;
+    let statesCovered = new Set();
+    for (station in stations) {
+      let covered = statesNeeded && stations[station];
+      if (covered.size > statesCovered.size) {
+        bestStation = station;
+        statesCovered = covered;
+      }
+    }
+    // console.log(statesNeeded);
+    // statesNeeded.delete(statesCovered);
+    statesCovered.forEach(state => {
+      statesNeeded.delete(state);
+    });
+    // console.log(statesNeeded);
+    finalStations.add(bestStation);
+    delete stations[bestStation];
+  }
+  return finalStations;
+}
+
+console.log(findStations(statesNeeded, stations));
